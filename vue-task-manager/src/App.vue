15684fafc9681a107af23cb2373a8950235542d4
Ilarioni Mesh <ilia.meshvildishvili@gmail.com>
2021-11-09 15:47:38 +0100

    basic styles and clean up

---
+++ vue-task-manager/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+     
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 400px;
+  border: 2px solid steelblue;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+
+/* 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*/ 
+</style>
---
+++ vue-task-manager/src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+<header>
+    <h1></h1>
+</header>
+    
+</template>
